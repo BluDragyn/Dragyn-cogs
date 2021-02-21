36ba2b465450dfafe20ff3f1bc9ea7c55fa37a5e
BluDragyn <BluDragyn@users.noreply.github.com>
2021-02-21 13:56:29 -0500

    Update conversions.py

--- conv/conversions.py
+++ conv/conversions.py
@@ -14,11 +14,11 @@ _ = Translator("Converters", __file__)
 
 
 @cog_i18n(_)
-class Converters(commands.Cog):
-    """Some converters."""
+class Conversions(commands.Cog):
+    """Various conversions"""
 
-    __author__ = "Predä"
-    __version__ = "0.3.8"
+    __author__ = "BluDragyn"
+    __version__ = "0.1.0"
 
     async def red_delete_data_for_user(self, **kwargs):
         """Nothing to delete."""
@@ -28,8 +28,44 @@ class Converters(commands.Cog):
         self.bot = bot
 
     def format_help_for_context(self, ctx: commands.Context) -> str:
-        """Thanks Sinbad!"""
         pre_processed = super().format_help_for_context(ctx)
         return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
 
-    @commands.group(aliases=["converter"])
+    @commands.group(aliases=["conversion"])
+    async def conv(self, ctx: commands.Context):
+    """Various conversion utilities."""
+
+    
+    @conv.group(aliases=["ctof"])
+    async def celsius(self, ctx: commands.Context):
+        """
+        Convert degree Celsius to Fahrenheit.
+        
+        Usage:
+        To Fahrenheit: `[p]ctof celsius fahrenheit`
+        """
+
+    @celsius.command(name="fahrenheit", aliases=["f"])
+    async def celsius_to_fahrenheit(self, ctx: commands.Context, temperature: float):
+        fahrenheit = round((temperature * 1.8) + 32, 1)
+        msg = _("{temp:,}° Celsius is equal to {f:,}° Fahrenheit.").format(
+            temp=temperature, f=fahrenheit
+        )
+        await ctx.send(msg)
+
+    @conv.group(aliases=["ftoc"])
+    async def fahrenheit(self, ctx: commands.Context):
+        """
+        Convert Fahrenheit degree to Celsius.
+
+        Usage:
+        To Celsius: `[p]ftoc fahrenheit celsius`
+        """
+
+    @fahrenheit.command(name="celsius", aliases=["c"])
+    async def fahrenheit_to_celsius(self, ctx: commands.Context, temperature: float):
+        celsius = round((temperature - 32) / 1.8, 1)
+        msg = _("{temp:,}° Fahrenheit is equal to {c:,}° Celsius.").format(
+            temp=temperature, c=celsius
+        )
+        await ctx.send(msg)
