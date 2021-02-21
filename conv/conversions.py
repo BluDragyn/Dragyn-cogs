cf1f95bde33ed12cf0e2be3f457031eaf1f9d936
BluDragyn <BluDragyn@users.noreply.github.com>
2021-02-21 13:48:51 -0500

    Create conversions.py

---
+++ conv/conversions.py
@@ -0,0 +1,35 @@
+import discord
+
+from redbot.core.bot import Red
+from redbot.core import commands
+from redbot.core.i18n import Translator, cog_i18n
+from redbot.core.utils.chat_formatting import humanize_timedelta
+
+from typing import Union
+from datetime import datetime
+
+import contextlib
+
+_ = Translator("Converters", __file__)
+
+
+@cog_i18n(_)
+class Converters(commands.Cog):
+    """Some converters."""
+
+    __author__ = "Predä"
+    __version__ = "0.3.8"
+
+    async def red_delete_data_for_user(self, **kwargs):
+        """Nothing to delete."""
+        return
+
+    def __init__(self, bot: Red):
+        self.bot = bot
+
+    def format_help_for_context(self, ctx: commands.Context) -> str:
+        """Thanks Sinbad!"""
+        pre_processed = super().format_help_for_context(ctx)
+        return f"{pre_processed}\n\nAuthor: {self.__author__}\nCog Version: {self.__version__}"
+
+    @commands.group(aliases=["converter"])
